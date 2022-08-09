4e56a3a8768944108c3942ce8cccdedf5b603a9f
mephyreji <110896643+mephyreji@users.noreply.github.com>
2022-08-09 15:03:05 +0530

    First commit

---
+++ forlist.py
@@ -0,0 +1,31 @@
+#for_list=[1,2,3,4,5,6,7,8]
+#for x in for_list
+ #   print(x)
+  #  print(x*x)
+   # print(x*x*x)
+#n=[1,2,3,4,5]
+#for i in range(1,len(n)):
+ #   print(n[i])
+newlist=[1,2,34,56,66]
+print(newlist)
+newlist.clear()
+print(newlist)
+thislist=["apple","banana","cherry",]
+print(thislist)
+print(thislist.sort())
+print(thislist.sort(reverse=True))
+thislist.sort(reverse=True)
+print(thislist)
+thislist.sort()
+list1=["za","s","u"]
+list2=["rt","wee","hjf"]
+list1=["ooa","eryis","drtyuu"]
+list4=list1
+list4.append('188')
+print(list1)
+list6 = list1.copy()#it is used to copy the value bt it do not change the value list one
+list6.append("200")
+print(list6)
+
+
+
