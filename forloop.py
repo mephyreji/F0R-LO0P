f6b4863e073deca403f62cdb1e9b3df1638d7d2f
mephyreji <110896643+mephyreji@users.noreply.github.com>
2022-08-09 15:19:10 +0530

    second commit

---
+++ forloop.py
@@ -0,0 +1,10 @@
+#list=[1,2,3,4,5,6,7,8,9,10]
+#for i in list:
+#print(i)
+# = 10
+#if num%5==0:
+ #   print("num is divisiable by 5")
+p=[1,2,3,4,5,6,7,8,9]
+   for num in p:
+      if num % 2==0;
+        print(num, end="")
